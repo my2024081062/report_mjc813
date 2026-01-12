783d38853f13782115429e35717d388a43f8f6c5
my2024081062 <ish0817@naver.com>
2026-01-12 14:08:14 +0900

    js 이름 바꿈

--- HTML_0112/js/gameCharacter.js
+++ HTML_0112/js/gameCharacter.js
@@ -1,5 +1,5 @@
 class gameCharacter {
-  static id = 2;
+  static id = 1;
   #characters = [
     {
       id: 1, name: "신사임걸", cls: "마법사", sx: "여자",
@@ -23,105 +23,79 @@ class gameCharacter {
       birthDate: $("#birthDate").val()
     }
   }
-  printList() { //게임 리스트를 배열에서 가져와 화면에 출력
-    $("#characters").empty();
-    this.#characters.sort((a, b) => a.id - b.id);
-    for (let character of this.#characters) {
-      $("#characters").append(this.printRow(character));
-    }
+
+  insertGameCharacter(){
+    //조건문 검사
+    
+    //게임 캐릭터 생성해서 배열에 푸쉬
+    let newCharacter = this.createGameCharacter(`forInsert`);
+    this.#characters.push(newCharacter);
+  }
+  
+  updateGameCharacter(){
+    //조건문 검사
+
+    //아이디 검색해서 일치하는 배열의 객체를 수정함
+    let updateCharacter = this.createGameCharacter(`forUpdate`);
+    let findIndex = this.#characters.findIndex((character)=>{
+      return character.id *1 === $("#id").val() *1;
+    });
+    if (findIndex===-1)
+      return;
+    this.#characters.with(findIndex,updateCharacter);
   }
 
-  printRow(character) { //화면에 출력될 배열요소를 html째로 가져옴
-    let html = `
-      <div class="listDataRow">
-          <div class="listItem">
-              <div class="itemData text-wrapper">
-                  ${character.name}
-              </div>
-          </div>
-          <div class="listItem">
-              <div class="itemData text-wrapper">${character.cls}</div>
-          </div>
-          <div class="listItem">
-              <div class="itemData text-wrapper">${character.sx}</div>
-          </div>
-          <div class="listItem">
-              <div class="itemData text-wrapper">${character.hp}</div>
-          </div>
-          <div class="listItem">
-              <div class="itemData text-wrapper">${character.mp}</div>
-          </div>
-          <div class="listItem">
-              <div class="itemData text-wrapper">
-                  ${character.birthDate}
-              </div>
-          </div>
+  deleteGameCharacter(){
+    //조건문 검사
+
+    //아이디 검색해서 일치하는 배열의 객체를 삭제
+    let findIndex = this.#characters.findIndex((character)=>{
+      return character.id *1 === $("#id").val() *1;
+    });
+    if (findIndex===-1)
+      return;
+    this.#characters.splice(findIndex,1);
+  }
+
+  showCharacters(character){
+    return `
+    <div class="listDataRow">
+      <div class="listItem">
+        <div class="itemData text-wrapper">${character.name}</div>
       </div>
+      <div class="listItem">
+        <div class="itemData text-wrapper">${character.cls}</div>
+      </div>
+      <div class="listItem">
+        <div class="itemData text-wrapper">${character.sx}</div>
+      </div>
+      <div class="listItem">
+        <div class="itemData text-wrapper">${character.hp}</div>
+      </div>
+      <div class="listItem">
+        <div class="itemData text-wrapper">${character.mp}</div>
+      </div>
+      <div class="listItem">
+        <div class="itemData text-wrapper">${character.birthDate}</div>
+      </div>
+    </div>
     `;
-    return html;
   }
 
-
-  insertGameCharacter() { //추가
-    let insertGameCharacter = this.createGameCharacter();
-    $("#img").attr(`src`, `${insertGameCharacter.src}`);
-    this.#characters.push(insertGameCharacter);
-  }
-
-  updateGameCharacter() { //수정
-    let updateGameCharacter = this.createGameCharacter();
-    let findIndex = this.#characters.findIndex((character) => {
-      return character.id * 1 === updateGameCharacter.id * 1;
-    })
-    if (findIndex === -1)
-      return;
-    else
-      this.#characters = this.#characters.with(findIndex, updateGameCharacter);
-  }
-
-  deleteStudent() { //삭제
-    let findIndex = this.#characters.findIndex((character) => {
-      return character.id * 1 === $("#id").val() * 1;
-    })
-    if (findIndex === -1)
-      return;
-    else
-      this.#characters.splice(findIndex, 1);
-  }
-
-  clearInput() { //완료후 입력창 클리어
-    $("#title").val('');
-    $("#genre").val('');
-    $("#grade").val('');
-    $("#price").val('');
-    $("#src").val('');
-  }
-
-  setInputData(item) { //클릭된 상품으로 입력창 초기화
-    $("#title").val(`${item.title}`);
-    $("#genre").val(`${item.genre}`);
-    $("#grade").val(`${item.grade}`);
-    $("#price").val(`${item.price}`);
-    $("#src").val(`${item.src}`);
-    $("#img").attr(`src`, `${item.src}`);
-
-  }
-
-  printItem(title) { //클릭된 상품을 배열에서 찾기
-    let findItem = this.#characters.find((item) => item.title * 1 === title * 1);
-    if (findItem === undefined)
-      return;
-    else
-      this.setInputData(findItem);
+  print(){
+    $(".listDataBlock").empty();
+    for (let character of this.#characters) {
+      $(".listDataBlock").append(this.showCharaters(character));
+    }
   }
 }
 
 $(() => {
   let character = new gameCharacter();
-  nint.print();
+  character.print();
   $("#insert").click(function (e) {
     e.preventDefault();
-    character.insertGameCharacter();
+    character.insertGameCharater();
     character.print();
     character.clearInput();
   });
@@ -140,7 +114,7 @@ $(() => {
     character.clearInput();
   });
 
-  $(document).on(`click`, ".character", function (e) {
+  $(document).on(`click`, ".game", function (e) {
     nint.printItem($(e.currentTarget).children().first().text());
   });
 })
