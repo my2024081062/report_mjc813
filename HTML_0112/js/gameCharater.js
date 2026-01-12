203a42c4c5314b0ade7265e0dd3a647f34bc2465
my2024081062 <ish0817@naver.com>
2026-01-12 12:14:19 +0900

    Update gameCharater.js

--- HTML_0112/js/gameCharater.js
+++ HTML_0112/js/gameCharater.js
@@ -1,3 +1,14 @@
 class gameCharater {
-  
+  #name=""; #cls=""; #sx="";
+  #hp=0; #mp=0;
+  #str=0; #int=0; #dex=0; #lux=0;
+  #birthDate="";
+
+  constructor(name,cls,sx,hp,mp,str,int,dex,lux,birthDate){
+    this.#name = name; this.#cls = cls; this.#sx = sx;
+    this.#hp = hp; this.#mp=mp; 
+    this.#str = str; this.#int = int; this.#dex = dex; this.#lux = lux;
+    this.#birthDate = birthDate;
+  }
+
 }
