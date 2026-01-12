7c599738eb18d019d0884450ca89184f48cf5921
my2024081062 <ish0817@naver.com>
2026-01-12 12:38:07 +0900

    자료 저장을 객체를 담은 배열로 바꿈

--- HTML_0112/js/gameCharater.js
+++ HTML_0112/js/gameCharater.js
@@ -1,36 +1,57 @@
 class gameCharater {
-  #name=""; #cls=""; #sx="";
-  #hp=0; #mp=0;
-  #str=0; #int=0; #dex=0; #lux=0;
-  #birthDate="";
+  static id = 1;
+  #charaters = [
+    {
+      id: NaN, name: "", cls: "", sx: "",
+      hp: 0, mp: 0,
+      str: 0, int: 0, dex: 0, lux: 0,
+      birthDate: ""
+    }
+  ];
 
-  createGameCharater(){
-    this.#name = $("#name").val();
-    this.#cls = $("#cls").val();
+  createGameCharater(forInsert) {
+    return {
+      id: forInsert === `forInsert` ? gameCharater.id++ : {},
+      name: $("#name").val(),
+      sx: $("#sx").val(),
+      hp: $("#hp").val(),
+      mp: $("#mp").val(),
+      str: $("#str").val(),
+      int: $("#int").val(),
+      dex: $("#dex").val(),
+      lux: $("#lux").val(),
+      birthDate: $("#birthDate").val()
+    }
   }
 
 }
 
 $(() => {
+  let charater = new gameCharater();
+  nint.print();
   $("#insert").click(function (e) {
     e.preventDefault();
-    nint.addGameCharater();
-    nint.print();
-    nint.clearInput();
+    charater.insertGameCharater();
+    charater.print();
+    charater.clearInput();
   });
 
   $("#update").click(function (e) {
     e.preventDefault();
-    nint.updateGameCharater();
-    nint.print();
-    nint.clearInput();
+    charater.updateGameCharater();
+    charater.print();
+    charater.clearInput();
   });
 
   $("#delete").click(function (e) {
     e.preventDefault();
-    nint.deleteGameCharater();
-    nint.print();
-    nint.clearInput();
+    charater.deleteGameCharater();
+    charater.print();
+    charater.clearInput();
+  });
+
+  $(document).on(`click`, ".game", function (e) {
+    nint.printItem($(e.currentTarget).children().first().text());
   });
 })
 
