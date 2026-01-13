c570bbf42f16dd5016ce6a76726dc91a5812806b
my2024081062 <ish0817@naver.com>
2026-01-13 11:09:13 +0900

    js필요기능 완성

--- HTML_0112/js/gameCharacter.js
+++ HTML_0112/js/gameCharacter.js
@@ -13,7 +13,7 @@ class gameCharacter {
     return {
       id: forInsert === `forInsert` ? gameCharacter.id++ : $("#id").val(),
       name: $("#name").val(),
-      cls:$("#cls").val(),
+      cls: $("#cls").val(),
       sx: $("input:radio[name=sx]:checked").val(),
       hp: $("#hp").val(),
       mp: $("#mp").val(),
@@ -25,67 +25,74 @@ class gameCharacter {
     }
   }
 
-  checkInputValueIsError(forInsert){
-    if(forInsert === `forInsert` && this.#characters.some((character) => {
+  checkInputValueIsError(forInsert) {
+    if (forInsert === `forInsert` && this.#characters.some((character) => {
       return character.id * 1 === $("#id").val() * 1
-    })){
+    })) {
       alert(`아이디가 중복되었습니다`);
       return true;
     }
-    if($("#name").val()<=1 || $("#name").val()>10){
+    if ($("#name").val().length < 2 || $("#name").val().length > 10) {
       alert(`이름은 2글자 이상 10글자 미만으로 해주세요`);
+      $("#name").focus();
       return true;
     }
-    if($(".inputNumber").val()<0 || $(".inputNumber").val()>200){
-      alert(`능력치는 0이상 200이하로 설정해주세요.`);
-      return true;
+    const statements = ["str", "int", "dex", "lux"];
+    for (const statement of statements) {
+      if ($(`#${statement}`).val() < 0 || $(`#${statement}`).val() > 200) {
+        alert(`${statement}를 0이상 200이하로 설정해주세요.`);
+        $(`#${statement}`).focus();
+        return true;
+      }
     }
   }
 
   insertGameCharacter() {
     //조건문 검사
-    if(this.checkInputValueIsError(`forInsert`)) return;
+    if (this.checkInputValueIsError(`forInsert`)) return;
     //게임 캐릭터 생성해서 배열에 푸쉬
     let newCharacter = this.createGameCharacter(`forInsert`);
     this.#characters.push(newCharacter);
+    this.clearInput();
   }
 
   updateGameCharacter() {
     //조건문 검사
-    if(this.checkInputValueIsError(``)) return;
+    if (this.checkInputValueIsError(``)) return;
     //아이디 검색해서 일치하는 배열의 객체를 수정함
     let updateCharacter = this.createGameCharacter(`forUpdate`);
     let findIndex = this.findCharacterIndex($("#id").val());
     if (findIndex === -1)
       return;
     else this.#characters = this.#characters.with(findIndex, updateCharacter);
+    this.clearInput();
   }
 
   deleteGameCharacter() {
-    //조건문 검사
-    if(this.checkInputValueIsError(``)) return;
+    //조건문 검사 필요없음
     //아이디 검색해서 일치하는 배열의 객체를 삭제
     let findIndex = this.findCharacterIndex($("#id").val());
     if (findIndex === -1)
       return;
     else this.#characters.splice(findIndex, 1);
+    this.clearInput();
   }
 
-  changeCls(cls){
-    if(cls==="W")
+  changeCls(cls) {
+    if (cls === "W")
       return "전사";
-    if(cls==="M")
+    if (cls === "M")
       return "마법사";
-    if(cls==="A")
+    if (cls === "A")
       return "궁수";
-    if(cls==="T")
+    if (cls === "T")
       return "도적";
   }
 
-  changeSx(sx){
-    if(sx==="M")
+  changeSx(sx) {
+    if (sx === "M")
       return "남자";
-    if(sx==="F")
+    if (sx === "F")
       return "여자";
   }
 
@@ -117,19 +124,6 @@ class gameCharacter {
     `;
   }
 
-  printListAttack(id){
-    $("#attStrTarget").empty();
-    $("#attStrTarget").append(`<option value = "0">선택하세요</option>`);
-    $("#attIntTarget").empty();
-    $("#attIntTarget").append(`<option value = "0">선택하세요</option>`);
-
-    for (let character of this.#characters) {
-      if(character.id *1 === id*1) continue;
-      $("#attStrTarget").append(`<option value = "${character.id}">${character.name}</option>`);
-      $("#attIntTarget").append(`<option value = "${character.id}">${character.name}</option>`);
-    }
-  }
-
   printListHtml() {
     $(".listDataBlock").empty();
     for (let character of this.#characters) {
@@ -137,6 +131,19 @@ class gameCharacter {
     }
   }
 
+  printListAttack(id) {
+    $("#attStrTarget").empty();
+    $("#attStrTarget").append(`<option value = "0">선택하세요</option>`);
+    $("#attIntTarget").empty();
+    $("#attIntTarget").append(`<option value = "0">선택하세요</option>`);
+
+    for (let character of this.#characters) {
+      if (character.id * 1 === id * 1) continue;
+      $("#attStrTarget").append(`<option value = "${character.id}">${character.name}</option>`);
+      $("#attIntTarget").append(`<option value = "${character.id}">${character.name}</option>`);
+    }
+  }
+  
   clearInput() { //완료후 입력창 클리어
     $("#id").val(0);
     $("#name").val("");
@@ -167,7 +174,7 @@ class gameCharacter {
 
     this.printListAttack(character.id);
   }
-  
+
   printItem(id) { //클릭된 캐릭터 배열에서 찾기
     let findItem = this.#characters.find((character) => character.id * 1 === id * 1);
     if (findItem === undefined)
@@ -176,50 +183,52 @@ class gameCharacter {
       this.setInputData(findItem);
   }
 
-  findCharacter(id){
-    return this.#characters.find((character)=>{
-      return character.id *1 === id *1;
+  findCharacter(id) {
+    return this.#characters.find((character) => {
+      return character.id * 1 === id * 1;
     });
   }
-
-  findCharacterIndex(id){
-    return this.#characters.findIndex((character)=>{
-      return character.id *1 === id *1;
+  findCharacterIndex(id) {
+    return this.#characters.findIndex((character) => {
+      return character.id * 1 === id * 1;
     });
   }
-
-  attackStr(){
-    if($("#attStrTarget").val()*1 === 0 || this.findCharacterIndex($("#id").val()) === -1) return;
-    else this.findCharacter($("#attStrTarget").val()).hp -= this.findCharacter($("#id").val()).str;
-  }
-
-  attackInt(){
-    if($("#attIntTarget").val()*1 === 0 || this.findCharacterIndex($("#id").val()) === -1) return;
-    let tmp = this.findCharacter($("#id").val()).mp - this.findCharacter($("#id").val()).int;
-    if(this.findCharacter($("#id").val()).mp === 0){
-      alert(`mp가 0이라 INT공격을 할 수 없습니다.`);
-      return;
-    }
-    else if(tmp<0){
-      alert(`mp가 남은 만큼 공격했습니다.`);
-      this.findCharacter($("#attIntTarget").val()).hp -= this.findCharacter($("#id").val()).mp;
-      this.findCharacter($("#id").val()).mp = 0;
-    }
-    else {
-      this.findCharacter($("#attIntTarget").val()).hp -= this.findCharacter($("#id").val()).int;
-      this.findCharacter($("#id").val()).mp -= this.findCharacter($("#id").val()).int;
-    }
-
-    if(this.findCharacter($("#attIntTarget").val()).hp<0){
-      let findIndex = this.findCharacterIndex($("#attIntTarget").val());
+  checkTargetHp(targetId, attackId){
+    if (this.findCharacter(targetId).hp <= 0) {
+      let findIndex = this.findCharacterIndex(targetId);
       if (findIndex === -1)
         return;
       else {
         this.#characters.splice(findIndex, 1);
-        character.printListHtml();
-        character.printListAttack($("#id").val());
+        this.printListHtml();
+        this.printListAttack(attackId);
       }
     }
+  }
+
+  attackStr() {
+    const targetId = $("#attStrTarget").val() * 1;
+    const attackId = $("#id").val() * 1;
+    if (this.findCharacter(targetId) === undefined || this.findCharacter(attackId) === undefined) 
+      return;
+    else 
+      this.findCharacter(targetId).hp -= this.findCharacter(attackId).str;
+    this.checkTargetHp(targetId, attackId);
+  }
+  attackInt() {
+    const targetId = $("#attIntTarget").val() * 1;
+    const attackId = $("#id").val() * 1;
+    if (this.findCharacter(targetId) === undefined || this.findCharacter(attackId) === undefined) 
+      return;
+    else if (this.findCharacter(attackId).mp < this.findCharacter(attackId).int) {
+      alert(`남은 mp가 int보다 부족하여 공격할 수 없습니다.`);
+      return;
+    }
+    else {
+      this.findCharacter(targetId).hp -= this.findCharacter(attackId).int;
+      this.findCharacter(attackId).mp -= this.findCharacter(attackId).int;
+    }
+    this.checkTargetHp(targetId, attackId);
   }
 }
 
@@ -227,12 +236,12 @@ $(() => {
   let character = new gameCharacter();
   character.printListHtml();
   character.printListAttack();
+  
   $("#btnAdd").click(function (e) {
     e.preventDefault();
     character.insertGameCharacter();
     character.printListHtml();
     character.printListAttack();
-    character.clearInput();
   });
 
   $("#btnUpt").click(function (e) {
@@ -240,20 +249,19 @@ $(() => {
     character.updateGameCharacter();
     character.printListHtml();
     character.printListAttack();
-    character.clearInput();
   });
+
   $("#btnDel").click(function (e) {
     e.preventDefault();
     character.deleteGameCharacter();
     character.printListHtml();
     character.printListAttack();
-    character.clearInput();
   });
 
   $(document).on(`click`, ".listDataRow", function (e) {
     character.printItem($(e.currentTarget).children().last().text());
   });
-  
+
   $(document).on(`click`, "#btnAttStr", function (e) {
     character.attackStr();
     character.printListHtml();
